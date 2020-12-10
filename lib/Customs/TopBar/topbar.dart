8e9fcc32904fd1ba79fae43b078766f3d2c271c3
Cullen Lee <a410615903@gmail.com>
2020-12-10 01:36:32 -0800

    Optimized viewers.

--- lib/Customs/TopBar/topbar.dart
+++ lib/Customs/TopBar/topbar.dart
@@ -15,59 +15,57 @@ class _TopBarState extends State<TopBar> {
     return Container(
         width: _width,
         height: 50,
-        child: Card(
-            color: Colors.black12,
-            child: Row(children: [
-              const SizedBox(width: 10),
-              IconButton(
-                icon: Icon(Icons.add),
-                iconSize: 30,
-                color: Colors.white,
-                onPressed: () => {},
-              ),
+        color: Colors.black12,
+        child: Row(children: [
+          const SizedBox(width: 10),
+          IconButton(
+            icon: Icon(Icons.add),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () => {},
+          ),
 
-              // width: 140,
-              IconButton(
-                  icon: Icon(Icons.access_time, semanticLabel: "Reschedule"),
-                  color: Colors.red,
-                  iconSize: 30,
-                  onPressed: () => {}
-                  // Text(
-                  //   "Reschedule",
-                  //   style: TextStyle(
-                  //       fontSize: 16,
-                  //       fontWeight: FontWeight.w600,
-                  //       color: Colors.red),
-                  // )
-                  // style: TextButton.styleFrom(shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5)),backgroundColor: Colors.red),
-                  ),
-              IconButton(
-                  icon:
-                      Icon(Icons.assignment_turned_in, semanticLabel: "Check"),
-                  color: Colors.white,
-                  iconSize: 30,
-                  onPressed: () => {}),
-              IconButton(
-                  icon: Icon(Icons.refresh, semanticLabel: "Refresh"),
-                  color: Colors.green,
-                  iconSize: 30,
-                  onPressed: () => {}),
-              Expanded(
-                child: Center(child: Timer()),
+          // width: 140,
+          IconButton(
+              icon: Icon(Icons.access_time, semanticLabel: "Reschedule"),
+              color: Colors.red,
+              iconSize: 30,
+              onPressed: () => {}
+              // Text(
+              //   "Reschedule",
+              //   style: TextStyle(
+              //       fontSize: 16,
+              //       fontWeight: FontWeight.w600,
+              //       color: Colors.red),
+              // )
+              // style: TextButton.styleFrom(shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5)),backgroundColor: Colors.red),
               ),
-              IconButton(
-                icon: Icon(Icons.archive_sharp),
-                iconSize: 30,
-                color: Colors.white,
-                onPressed: () => {},
-              ),
-              IconButton(
-                icon: Icon(Icons.settings),
-                iconSize: 30,
-                color: Colors.white,
-                onPressed: () => {},
-              ),
-              const SizedBox(width: 10),
-            ])));
+          IconButton(
+              icon: Icon(Icons.assignment_turned_in, semanticLabel: "Check"),
+              color: Colors.white,
+              iconSize: 30,
+              onPressed: () => {}),
+          IconButton(
+              icon: Icon(Icons.refresh, semanticLabel: "Refresh"),
+              color: Colors.green,
+              iconSize: 30,
+              onPressed: () => {}),
+          Expanded(
+            child: Center(child: Timer()),
+          ),
+          IconButton(
+            icon: Icon(Icons.archive_sharp),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () => {},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () => {},
+          ),
+          const SizedBox(width: 10),
+        ]));
   }
 }
